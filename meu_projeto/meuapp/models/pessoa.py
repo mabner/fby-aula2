0bf231c00a417aa73b054384629b5a90992f0042
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-06-29 22:02:47 -0300

    PessoaManager

--- meu_projeto/meuapp/models/pessoa.py
+++ meu_projeto/meuapp/models/pessoa.py
@@ -17,3 +17,34 @@ class Pessoa(models.Model):
                                      on_delete=models.SET_NULL,
                                      null=True)
     telefones = models.ManyToManyField(Telefone)
+
+
+class PessoaManager(models.Manager):
+
+    def obter_todas_pessoas(self):
+        return self.all()
+
+    def obter_familia(self):
+        pessoas = self.filter(sobrenome="Leite")
+        return pessoas
+
+    def obter_masculino(self):
+        masculino = self.filter(sexo="M")
+        return masculino
+
+    def obter_feminino(self):
+        feminino = self.filter(sexo="F")
+        return feminino
+
+    def obter_maioridade(self):
+        maiores = self.filter(idade__gte=18)
+        # gte -> greater than or equal to
+        return maiores
+
+    def obter_primeiro_cadastro(self):
+        primeiro = self.get(id=1)
+        return primeiro
+
+    def pessoas_m(self):
+        pessoas_m = self.filter(nome__startswith="M")
+        return pessoas_m
